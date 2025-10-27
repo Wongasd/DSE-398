ce7613ebe52215372df6d6c6b6aaa4dee23a172f
Wong Park Kit <wongparkkit789@gmail.com>
2025-10-27 21:10:54 +0800

    Exported library.sql

--- backup/11-19-2024/log_out.php
+++
@@ -1,8 +0,0 @@
-<?php
-session_start();
-
-if(($_GET['action'] && $_GET['action'] == 'logout')){
-	session_destroy();
-	echo "<script>window.location.href='index.php';alert('log out');</script>";
-}
-?>
